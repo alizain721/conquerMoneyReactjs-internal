8e13981ce994c9a78c8dd2f9070a63445d7ed316
Yufan Lou <2263580+louy2@users.noreply.github.com>
2020-10-17 21:24:47 -0400

    test(App): add test for SplashScreen
    trying to use fake timer to get past SplashScreen
    don't know why using act doesn't work

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, act, cleanup } from '@testing-library/react';
 import App from './App';
 
-test.skip('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// about before and after
+// https://jestjs.io/docs/en/setup-teardown
+
+// use fake timers in tests and return to real timers after
+beforeEach(() => {
+  jest.useFakeTimers();
+})
+
+afterEach(() => {
+  // must flush pending timers before returning to real timers
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
+  cleanup();
+})
+
+describe('SplashScreen', () => {
+  it('contains wait message', () => {
+    render(<App />);
+    // look for wait message
+    const loadingText = screen.queryByText(/Wait a moment/i);
+    // assert wait message exists
+    expect(loadingText).toBeInTheDocument();
+  });
+  it('goes away after 2 seconds', () => {
+    render(<App />);
+    // pass 2 seconds
+    act(() => jest.advanceTimersByTime(2000));
+    // look for wait message
+    const loadingText = screen.queryByText(/Wait a moment/i);
+    // assert wait message disappears
+    expect(loadingText).not.toBeInTheDocument();
+  })
 });
