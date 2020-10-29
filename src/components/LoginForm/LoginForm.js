b644c4dff8f2bb48272834329c45da2b7a230eec
roypark <roypark19@gmail.com>
2020-10-29 11:07:52 -0700

    change logo

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./LoginForm.css";
 import { API_BASE_URL, API_URL } from "../../constants/apiConstants";
 import { withRouter } from "react-router-dom";
-import avatar from "../../img/SpartanLogo.jpg";
+import avatar from "../../img/Logo_v3.png";
 
 import Cookie from "js-cookie";
 
