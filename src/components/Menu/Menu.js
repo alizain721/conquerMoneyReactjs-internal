a4c3fd11f1d9f541d8708b92b0a0c2ed9e96b2c6
smurf211 <mspadaro@student.sjcny.edu>
2020-11-05 21:37:59 -0500

    logout fixed, reg page changes, account page changes

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import { useHistory } from "react-router-dom";
+import MenuItem from "@material-ui/core/MenuItem";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { API_URL, API_LOGOUT } from "../../constants/apiConstants.js";
+import Cookie from "js-cookie";
+import axios from "axios";
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  var history = useHistory();
+  const logoutReq = () => {
+    const token = Cookie.get("token");
+    const payload = {
+      token: token,
+    };
+    axios
+      .post(API_URL + API_LOGOUT, payload)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("logout response received");
+          history.push("/"); //returns to home
+        } else {
+          console.log("Error while attempting logout");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    logoutReq();
+    Cookie.set("token", { expires: 0.0 });
+    console.log("logout complete");
+  };
+
+  return (
+    <div>
+      <BottomNavigationAction
+        label="Settings"
+        value="settings"
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        icon={<SettingsIcon />}
+        showLabel={true}
+      />
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
---
+++ src/constants/apiConstants.js
@@ -0,0 +1,23 @@
+export const API_BASE_URL = "/signin";
+export const API_REG_URL = "/signup";
+export const API_ADDCARD_URL = "/addcard";
+export const API_GETPOST_URL = "/getpost"
+export const API_POPACCOUNTS_URL = "/popaccounts";
+export const API_GETBALANCECREDIT_URL = "/getbalancecredit";
+export const API_GENTILES_URL = "/gentiles";
+export const TRANS_URL = "/transactions";
+export const GET_ACCESS_URL = "/get_access_token";
+export const API_DELETEACCOUNT_URL = "/deleteaccount";
+export const API_TRANS = "/transactions";
+export const API_GET_TRANS = "/get_transactions";
+export const API_GET_ACCOUNTS = "/get_accounts";
+export const API_ADDPOST_URL = "/addpost";
+export const API_GET_CASH = "/get_cash";
+export const API_GET_CARDS = "/get_cards";
+export const API_GET_LOANS = "/get_loans";
+export const API_GET_HEADERDASH = "/get_headerdash";
+export const API_GET_PA = "/get_purchase_analysis";
+export const API_LOGOUT = "/logout";
+
+export const API_URL = "http://localhost:8080/api/auth";
+export const API_PUB_URL = "http://localhost:8080/api/pub";
