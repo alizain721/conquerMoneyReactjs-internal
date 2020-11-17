550777c1635004aaa9db1665f9f1d6298d60e14e
djhanna01 <71840022+djhanna01@users.noreply.github.com>
2020-11-17 12:30:23 -0500

    Setting up Profile Page

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,29 @@
+import "./Profile.css";
+
+import React, { Component } from "react";
+import axios from "axios";
+import Cookie from "js-cookie";
+
+import { API_GENTILES_URL, API_URL } from "../../constants/apiConstants";
+import { withRouter, Link } from "react-router-dom";
+import Tile from "../Tile/Tile.js";
+
+class Profile extends Component{
+    constructor(){
+        super();
+
+        this.state = {
+            successMessage = null,
+            tiles: [],
+            tileList: [],
+            title: "test",
+            description: "something",
+        };
+    }
+
+    render(){
+        return (
+            <div classname="profilePage"></div>
+        )
+    }
+}
