66177ceb2207e05de1a061b9696395363f89e2e3
Hanyia Ahmed <hahmed5@u.rochester.edu>
2021-07-08 18:17:52 -0400

    tools button added

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,44 @@
+import "./Settings.css";
+
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import Cookie from "js-cookie";
+import { API_URL, API_LOGOUT } from "../../constants/apiConstants.js";
+import { withRouter, Link } from "react-router-dom";
+
+class Dashboard extends Component {
+    constructor() {
+        super();
+    }
+
+    logoutReq = () => {
+      const token = Cookie.get("token");
+      const payload = {
+        token: token,
+      };
+      axios
+        .post(API_URL + API_LOGOUT, payload)
+        .then((response) => {
+          if (response.status === 200) {
+            console.log("logout response received");
+            
+          } else {
+            console.log("Error while attempting logout");
+          }
+          Cookie.remove("token");
+          history.push("/"); //returns to home
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        console.log("logout complete");
+    };
+  
+    render() {
+      return (
+        div 
+      );
+    }
+}
+
