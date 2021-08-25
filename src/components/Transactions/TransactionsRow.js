3a082f2fb69f97cea795a5228ebc6ad27feb3edc
Michelle Wong <michellewong@michelles-air.myfiosgateway.com>
2021-08-24 21:13:06 -0400

    AAdded default logos for different categories

--- src/components/Transactions/TransactionsRow.js
+++ src/components/Transactions/TransactionsRow.js
@@ -14,15 +14,48 @@ import axios from "axios";
 import Cookie from "js-cookie";
 import "./Transactions.css";
 import "./Transactions.js"
+import bankFees from "../../img/transactionLogos/Bank Fees.png";
+import foodAndDrink from "../../img/transactionLogos/Food and Drink.png";
+import interest from "../../img/transactionLogos/Interest.png";
+import payment from "../../img/transactionLogos/Payment.png";
+import recreation from "../../img/transactionLogos/Recreation.png";
+import shops from "../../img/transactionLogos/Shops.png";
+import transfer from "../../img/transactionLogos/Transfer.png";
+import travel from "../../img/transactionLogos/Payment.png";
 
 class Transactions extends Component {
   constructor(props) {
     super(props);
 
+    this.addDefaultSrc = this.addDefaultSrc.bind(this)
+
   }
 
   addDefaultSrc(ev){
-    ev.target.src = 'https://i.pinimg.com/474x/14/04/8d/14048d06bdf4c1d0f724370b7eabc45e.jpg';
+    var categ = this.props.category;
+    console.log(typeof(categ));
+    console.log(categ);
+    if (categ == null){
+      ev.target.src = 'https://i.pinimg.com/474x/14/04/8d/14048d06bdf4c1d0f724370b7eabc45e.jpg'
+    }else if (categ.startsWith('Bank Fees')){
+      ev.target.src = bankFees;
+    }else if (categ.startsWith('Food and Drink')){
+      ev.target.src = foodAndDrink;
+    }else if (categ.startsWith('Interest')){
+      ev.target.src = interest;
+    }else if (categ.startsWith('Payment')){
+      ev.target.src = payment;
+    }else if (categ.startsWith('Recreation')){
+      ev.target.src = recreation;
+    }else if (categ.startsWith('Shops')){
+      ev.target.src = shops;
+    }else if (categ.startsWith('Transfer')){
+      ev.target.src = transfer;
+    }else if (categ.startsWith('Travel')){
+      ev.target.src = travel;
+    }else{
+      ev.target.src = 'https://i.pinimg.com/474x/14/04/8d/14048d06bdf4c1d0f724370b7eabc45e.jpg'
+    }
     ev.onerror=null;
   }
 
